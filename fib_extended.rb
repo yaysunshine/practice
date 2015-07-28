fd9952f6f483ca97970a7f93939d190412f524df
yaysunshine <merida1592@gmail.com>
2015-07-27 21:42:38 -0700

    working one line code for fibonacci with explanation

--- fib_extended.rb
+++ fib_extended.rb
@@ -2,3 +2,15 @@
 
 # (Hint: use inject)
 
+def fibon(n)
+  # return 1 if n == 1 || n == 2
+  # return fib(n-1) + fib(n-2) if n > 2
+  # (5..10).inject { |sum, n| sum + n }
+  (1..10).inject( [0, 1] ) { |fib| fib << fib.last(2).inject(:+) }
+  # inject(initial) { |obj| block }
+  # .inject(:+) || .reduce(:+) --> add together
+
+  # (1..20).inject( [0, 1] ) { | fib | fib << fib.last(2).inject(:+) }
+end
+
+p fibon(10)
