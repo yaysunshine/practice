f0da9aec75e872633de99563b9c6cb98fc4d0040
yaysunshine <merida1592@gmail.com>
2015-07-27 21:48:18 -0700

    add roman.rb and add code from dbc challenge

---
+++ roman.rb
@@ -0,0 +1,11 @@
+def to_roman(num)
+  # Your code here
+end
+
+# Driver code... this should print out trues.
+
+puts to_roman(1) == "I"
+puts to_roman(3) == "III"
+puts to_roman(6) == "VI"
+
+# TODO: what other cases could you add to ensure your to_roman method is working?
