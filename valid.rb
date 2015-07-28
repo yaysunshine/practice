995bf4c267e9310edd0d47175ca64cf3122eabc8
yaysunshine <merida1592@gmail.com>
2015-07-27 20:09:01 -0700

    valid.rb has a working solution of what i think the question was asking

--- valid.rb
+++ valid.rb
@@ -1,3 +1,8 @@
 # is this valid?
 
-(a) {p a}["Hello world"]
+# (a) {p a}["Hello world"]
+
+a = ["Hello world"]
+p a
+
+# not sure what they're wanting to do with this code. I don't think it is valid. I gave a working solution to what I think they want to do.
